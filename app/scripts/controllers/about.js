2c12bce537dc635a26ad2560b9e6360ed846d175
antonio gomez del peso <antonio gomez del peso>
2017-03-12 17:46:25 +0100

    subir proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myOrganicerTotalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myOrganicerTotalApp
+ */
+angular.module('myOrganicerTotalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myOrganicerTotalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myOrganicerTotalApp
+ */
+angular.module('myOrganicerTotalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
